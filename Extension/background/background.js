e2ede7f1a3d6c8b5b76ec4ca0990f7703305d2ad
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-11-27 05:43:09 +0500

    better file structure, webpack workin

--- Extension/background/background.js
+++ Extension/background/background.js
@@ -1,8 +1,1 @@
-const ebayLister = new EbayLister();
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'createListing') {
-    ebayLister.createListing(request.productData).then(sendResponse);
-    return true; // Keep message channel open for async response
-  }
-});
+console.log('background.js');
