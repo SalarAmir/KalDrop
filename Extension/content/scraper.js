9b26cd1598728bf9efeb96ab671151669764ce0f
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2025-04-11 15:39:01 +0500

    scraper bug

--- Extension/content/scraper.js
+++ Extension/content/scraper.js
@@ -109,7 +109,7 @@ export class AliExpressScraper {
       });
       sliderElement.dispatchEvent(moveoverEvent);
       await new Promise(resolve => setTimeout(resolve, 300));
-      const [imageElement] = document.querySelectorAll('[class*="magnifier--image"] img');
+      const [imageElement] = document.getElementsByClassName('magnifier--image--EYYoSlr');
       if (!imageElement) {
         console.error('Image element not found');
         continue;
